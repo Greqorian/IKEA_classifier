dd1161d7933da5f13c9fc92e3a8aeff94e8726aa
Greq <greqorian@gmail.com>
2022-02-08 14:35:15 +0100

    fix bugs 2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from fastai.basics import load_learner
-from fastai.vision import open_image
+from fastai.vision.core import load_image
 from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
@@ -8,12 +8,12 @@ CORS(app, support_credentials=True)
 
 # load the learner
 learn = load_learner(path='./models', file='5items_model.pkl')
-classes = learn.data.classes
+classes = learn.dls.vocab
 
 
 def predict_single(img_file):
     'function to take image and return prediction'
-    prediction = learn.predict(open_image(img_file))
+    prediction = learn.predict(load_image(img_file))
     probs_list = prediction[2].numpy()
     return {
         'category': classes[prediction[1].item()],
